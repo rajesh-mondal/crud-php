a5a2b454333125b0bb9828b6bbe7a94caf2ac45e
Rajesh Mondal <rajesh01749@gmail.com>
2023-10-26 14:58:42 +0600

    project bootstraping done

---
+++ inc/templates/nav.php
@@ -0,0 +1,5 @@
+<p>
+    <a href="">All Students</a> |
+    <a href="">Add New Student</a> |
+    <a href="">Seed</a>
+</p>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Example</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+    <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
+    <style>
+        body {
+            margin-top: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="column column-60 column-offset-20">
+            <h2>CRUD Project</h2>
+            <p>A sample project to perform CRUD operations using plain files and PHP</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
